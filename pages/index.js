4d0d988a4bdf04a59217bf7a481d3d19a07c104b
Pascal Mayr <pascalmayr2@gmail.com>
2020-12-04 14:31:43 +0100

    updated the dashboard page

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,27 @@ import { Page,
   Button
 } from "@shopify/polaris";
 import {
-  CapitalMajor,
-  EditMajor
+  CapitalMajor
 } from '@shopify/polaris-icons';
 import Image from 'next/image';
 import '../styles/index_page.css';
+import Link from 'next/Link';
 
-const Index = () => {
+const Index = ({rows = []}) => {
   return (
   <Page
     fullWidth
-    title="Sales Boost Toolbox"
-    subtitle="All campaigns"
-    primaryAction={{content: 'New Campaign', disabled: false }}
-    secondaryActions={[{content: 'Free plan', disabled: true, icon: CapitalMajor}]}
+    title="All Campaigns"
+    subtitle="Create new campaigns and boost your sales."
+    primaryAction={
+      <Link href='/campaigns'>
+        <Button primary><span className='salestorm-add-campaign'>+</span> New Campaign</Button>
+      </Link>
+    }
+    secondaryActions={[
+      {content: 'Upgrade', disabled: false, url: '/pricing'},
+      {content: 'Free plan', disabled: true, icon: CapitalMajor},
+    ]}
   >
     <DataTable
       columnContentTypes={[
@@ -37,16 +44,15 @@ const Index = () => {
         ''
       ]}
       sortable={[true, true, true, true, true, true, false]}
-      rows={
-        /*[
-          ['Christmas', 'Timer', 'Product Page over the Product', 'active', '11.02.2021 17:30', '13.02.2021 17:30', <Button icon={EditMajor} />],
-          ['Easter', 'Timer', 'Product Page over the Product', 'inactive', '11.02.2021 17:30', '13.02.2021 17:30', <Button icon={EditMajor} />]
-        ]*/[]}
+      rows={rows}
     />
-    <div id="no-campaigns-image">
-      <Image src='/imagination.svg' alt="me" width="250" height="250" />
-      You have no campaigns yet.
-    </div>
+    {
+      rows.length === 0 &&
+      <div id="no-campaigns-image">
+        <Image src='/imagination.svg' alt="me" width="250" height="250" />
+        You have no campaigns yet.
+      </div>
+    }
   </Page>
   )
 };
