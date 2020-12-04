0ff1a5462b02522f305ecc5c0338ad9ff35f3809
Pascal Mayr <pascalmayr2@gmail.com>
2020-12-04 14:59:53 +0100

    renamed the global css file

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import fetch from "node-fetch";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import App, { Container } from "next/app";
+import App from "next/app";
 import { AppProvider } from "@shopify/polaris";
 import { Provider } from "@shopify/app-bridge-react";
 import Cookies from "js-cookie";
 import "@shopify/polaris/dist/styles.css";
 import translations from "@shopify/polaris/locales/en.json";
-import '../styles/global.css';
+import '../styles/_app.css';
 import Head from 'next/head';
 
 const client = new ApolloClient({
