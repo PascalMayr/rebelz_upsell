fd986086dfb85c633b69531cb17aba522baf09ca
Pascal Mayr <pascalmayr2@gmail.com>
2020-12-05 12:08:51 +0100

    removed alignjustify

--- pages/campaigns/index.js
+++ pages/campaigns/index.js
@@ -39,7 +39,7 @@ const Index = () => {
                 ],
                 toolbar:
                   'undo redo | fontselect | fontsizeselect | countdownTimerButton | emoticons | bold italic | forecolor backcolor | link | \
-                  alignleft aligncenter alignright alignjustify | \
+                  alignleft aligncenter alignright | \
                   removeformat',
                 font_formats: 'Andale Mono=andale mono,times;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Trebuchet MS=trebuchet ms,geneva;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats',
                 color_cols: 5,
