c154e0e0b898d455135f1075b3ff23f3676860a4
Pascal Mayr <pascalmayr2@gmail.com>
2020-12-18 21:40:00 +0100

    created a preview mobile desktop switch component

---
+++ components/preview_switch.js
@@ -0,0 +1,35 @@
+import '../styles/components_preview_switch.css';
+import { Button } from '@shopify/polaris';
+import {
+  DesktopMajor,
+  MobileMajor
+} from '@shopify/polaris-icons';
+import { useState } from 'react';
+
+const PreviewSwitch = ({onSwitch = () => {}}) => {
+  const [selected, setSelected] = useState('desktop');
+  const handleSwitch = (value) => {
+    setSelected(value);
+    onSwitch(value);
+  }
+  return (
+    <div className='salestorm-preview-switch'>
+      <Button
+        icon={DesktopMajor}
+        primary={selected === 'desktop'}
+        onClick={() => handleSwitch('desktop')}
+      >
+        Desktop
+      </Button>
+      <Button
+        icon={MobileMajor}
+        primary={selected === 'mobile'}
+        onClick={() => handleSwitch('mobile')}
+      >
+        Mobile
+      </Button>
+    </div>
+  )
+}
+
+export default PreviewSwitch
