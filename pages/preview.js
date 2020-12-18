d4293077c757044ef227aade94798d0c25a65187
Pascal Mayr <pascalmayr2@gmail.com>
2020-12-18 21:40:10 +0100

    created a preview page

---
+++ pages/preview.js
@@ -0,0 +1,12 @@
+import '../styles/pages_preview.css';
+import PreviewSwitch from '../components/preview_switch';
+
+const Preview = () => {
+  return (
+    <div>
+      <PreviewSwitch onSwitch={() => {}} />
+    </div>
+  );
+};
+
+export default Preview;
