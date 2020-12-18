dd850ea400277ffaa6199fd2bb0df9a652be0ea8
Pascal Mayr <pascalmayr2@gmail.com>
2020-12-18 21:27:48 +0100

    created a campaign name input field

--- pages/campaigns/index.js
+++ pages/campaigns/index.js
@@ -33,6 +33,7 @@ const Index = () => {
     },
     message: '<div bis_skin_checked="1" class="mce-content-body" contenteditable="true" style="position: relative;" spellcheck="false"><p style="text-align: center; color: #fff;" data-mce-style="text-align: center; color: #fff;"><span style="color: rgb(236, 240, 241); font-size: 18pt;" data-mce-style="color: #ecf0f1; font-size: 18pt;"><br data-mce-bogus="1"></span></p><p style="text-align: center; color: #fff;" data-mce-style="text-align: center; color: #fff;"><span style="color: rgb(236, 240, 241); font-size: 18pt;" data-mce-style="color: #ecf0f1; font-size: 18pt;">Tap the text to insert your </span></p><p style="text-align: center; color: #fff;" data-mce-style="text-align: center; color: #fff;"><span style="color: rgb(236, 240, 241); font-size: 18pt;" data-mce-style="color: #ecf0f1; font-size: 18pt;">campaign message 🤠</span></p></div>',
     published: false,
+    name: ''
   });
   const setCampaignProperty = useCallback(
     (value, id) => setCampaign({ ...campaign, [id]: value }),
@@ -122,6 +123,9 @@ const Index = () => {
       }}
     >
       <Card>
+        <Card.Section>
+          <TextField placeholder='Campaign name' onChange={value => setCampaignProperty(value, 'name')} value={campaign.name} />
+        </Card.Section>
         <Card.Section>
           <Layout>
             <Layout.Section>
