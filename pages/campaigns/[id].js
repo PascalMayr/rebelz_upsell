8ab367ec4a3343472d4d95107bc6ffa8498a91e8
Pascal Mayr <pascalmayr2@gmail.com>
2020-12-26 15:19:49 +0100

    created a campaign edit page

---
+++ pages/campaigns/[id].js
@@ -0,0 +1,17 @@
+import New from './new';
+import db from '../../server/db';
+
+export async function getServerSideProps(context) {
+  const data = db.query('SELECT * FROM campaigns WHERE id = $1', [context.params.id]);
+  return {
+    props: {
+      campaign: data.rows[0]
+    }
+  }
+}
+
+const Edit = ({campaign}) => {
+  return <New campaign={campaign} />
+}
+
+export default Edit
