4ccd59ff6aa05cee099d18e1c0b473aca6fadb37
Sascha Mayr <mayr.sascha@gmail.com>
2020-12-26 15:33:17 +0100

    Fixes

--- pages/campaigns/[id].js
+++ pages/campaigns/[id].js
@@ -2,7 +2,7 @@ import New from './new';
 import db from '../../server/db';
 
 export async function getServerSideProps(context) {
-  const data = db.query('SELECT * FROM campaigns WHERE id = $1', [context.params.id]);
+  const data = await db.query('SELECT * FROM campaigns WHERE id = $1', [context.params.id]);
   return {
     props: {
       campaign: data.rows[0]
