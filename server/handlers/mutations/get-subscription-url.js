9545ae3cdd66003390faaa5a293787f2f19ffcdf
Sascha Mayr <mayr.sascha@gmail.com>
2021-01-14 14:10:11 +0100

    Improve code for subscription mutation

--- server/handlers/mutations/get-subscription-url.js
+++ server/handlers/mutations/get-subscription-url.js
@@ -1,22 +1,14 @@
 import 'isomorphic-fetch';
 import { gql } from 'apollo-boost';
 
-export function RECURRING_CREATE(url) {
+export function RECURRING_CREATE() {
   return gql`
     mutation {
       appSubscriptionCreate(
           name: "Super Duper Plan"
-          returnUrl: "${url}"
+          returnUrl: $url: String!
           test: true
           lineItems: [
-          {
-            plan: {
-              appUsagePricingDetails: {
-                  cappedAmount: { amount: 10, currencyCode: USD }
-                  terms: "$1 for 1000 emails"
-              }
-            }
-          }
           {
             plan: {
               appRecurringPricingDetails: {
@@ -40,11 +32,9 @@ export function RECURRING_CREATE(url) {
 
 export const getSubscriptionUrl = async (ctx) => {
   const { client } = ctx;
-  const confirmationUrl = await client
-    .mutate({
-      mutation: RECURRING_CREATE(process.env.HOST),
-    })
-    .then((response) => response.data.appSubscriptionCreate.confirmationUrl);
-
-  return ctx.redirect(confirmationUrl);
+  const response = await client.mutate({
+    mutation: RECURRING_CREATE(),
+    variables: { url: process.env.HOST },
+  });
+  return ctx.redirect(response.data.appSubscriptionCreate.confirmationUrl);
 };
