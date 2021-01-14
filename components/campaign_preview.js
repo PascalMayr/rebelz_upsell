039ec487bde18c915f0a57b13788e7a79ef0f95a
Pascal Mayr <pascalmayr2@gmail.com>
2021-01-14 14:52:13 +0100

    not using tinymce anymore to style the popup text

--- components/campaign_preview.js
+++ components/campaign_preview.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { kebabCasify } from 'casify';
-import { Editor } from '@tinymce/tinymce-react';
 import '../styles/components_campaign_preview.css';
 import { gql } from 'apollo-boost';
 import { Query } from 'react-apollo';
@@ -14,28 +13,6 @@ const CampaignPreview = ({
   popupRef,
   setCampaignProperty,
 }) => {
-  const inlineEditorConfig = {
-    inline: true,
-    menubar: false,
-    plugins: [
-      'advlist autolink lists link charmap print preview anchor textcolor',
-      'searchreplace visualblocks code fullscreen',
-      'insertdatetime media table paste code emoticons link',
-    ],
-    toolbar:
-      'undo redo | fontselect | fontsizeselect | countdownTimerButton | emoticons | bold italic strikethrough | forecolor backcolor | link | \
-      alignleft aligncenter alignright | \
-      removeformat',
-    font_formats:
-      'Andale Mono=andale mono,times;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Trebuchet MS=trebuchet ms,geneva;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats',
-    color_cols: 5,
-    branding: false,
-    elementpath: false,
-    placeholder: 'Tap to insert your campaign message.',
-    draggable_modal: true,
-    toolbar_mode: 'wrap',
-    statusbar: false,
-  };
   const GET_PRODUCT_DETAILS = (id) => gql`
     query {
       product(id: "${id}") {
@@ -86,12 +63,6 @@ const CampaignPreview = ({
             id="salestorm-popup"
             ref={popupRef}
           >
-            <Editor
-              apiKey={TINY_MCE_API_KEY}
-              init={inlineEditorConfig}
-              value={campaign[campaignMessageKey]}
-              onEditorChange={(value) => setCampaignProperty(value, campaignMessageKey)}
-            />
             <br />
             {
               campaign.products.selling.map(product => (
--- next.config.js
+++ next.config.js
@@ -4,11 +4,10 @@ const withCSS = require('@zeit/next-css');
 const webpack = require('webpack');
 const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
 const nodeEnv = JSON.stringify(process.env.NODE_ENV);
-const tinyMceApiKey = JSON.stringify(process.env.TINY_MCE_API_KEY);
 
 module.exports = withCSS({
   webpack: (config) => {
-    const env = { API_KEY: apiKey, NODE_ENV: nodeEnv, TINY_MCE_API_KEY: tinyMceApiKey };
+    const env = { API_KEY: apiKey, NODE_ENV: nodeEnv};
     config.plugins.push(new webpack.DefinePlugin(env));
     return config;
   },
