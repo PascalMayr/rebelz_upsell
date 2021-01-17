e0464e83cb75859e4fb7fa1398fca6bc277bd136
Sascha Mayr <mayr.sascha@gmail.com>
2021-01-17 19:38:51 +0100

    Add cancel subscription mutation

---
+++ server/handlers/mutations/cancel-subscription.js
@@ -0,0 +1,24 @@
+import 'isomorphic-fetch';
+import { gql } from 'apollo-boost';
+
+export function CANCEL_RECURRING() {
+  return gql`
+    mutation($id: ID!) {
+      appSubscriptionCancel(id: $id) {
+        userErrors {
+          field
+          message
+        }
+      }
+    }
+  `;
+}
+
+export const cancelSubscription = async (ctx, id) => {
+  const { client } = ctx;
+  const response = await client.mutate({
+    mutation: CANCEL_RECURRING(),
+    variables: { id },
+  });
+  return response.data.appSubscriptionCancel;
+};
