86815c59f9e9d78640bb5e7aaf42475905b9a259
Sascha Mayr <mayr.sascha@gmail.com>
2021-01-18 10:04:24 +0100

    Implement subscriptions update webhook

--- server/server.js
+++ server/server.js
@@ -80,6 +80,37 @@ app.prepare().then(() => {
     ctx.status = 200;
   });
 
+  router.post('/webhooks/app_subscriptions/update', webhook, async (ctx) => {
+    const shop = ctx.request.headers['x-shopify-shop-domain'];
+    const {
+      name,
+      status,
+      admin_graphql_api_id: subscriptionId,
+    } = ctx.request.body.app_subscription;
+    const storeData = await db.query('SELECT * FROM stores WHERE domain = $1', [
+      shop,
+    ]);
+    const store = storeData.rows[0];
+    const configPlan = config.plans.find((p) => p.name === name);
+    if (status === 'ACTIVE') {
+      await db.query(
+        'UPDATE stores SET plan_name = $1, "subscriptionId" = $2, plan_limit = $3 WHERE domain = $4',
+        [name, subscriptionId, configPlan.limit, shop]
+      );
+    } else if (store.subscriptionId === subscriptionId) {
+      const freePlan = config.plans.find(
+        (p) => p.name === config.planNames.free
+      );
+      await db.query(
+        'UPDATE stores SET plan_name = NULL, "subscriptionId" = NULL, plan_limit = $1 WHERE domain = $2',
+        [freePlan.limit, shop]
+      );
+    }
+
+    ctx.body = {};
+    ctx.status = 200;
+  });
+
   server.use(
     session(
       {
