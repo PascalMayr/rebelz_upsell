658a0074f500f861a42e591c8f73ebc68cd7fd8f
Pascal Mayr <pascalmayr2@gmail.com>
2021-01-22 23:50:31 +0100

    fixed saving texts to the DB

--- server/migrations/committed/000013.sql
+++ server/migrations/committed/000013.sql
@@ -1,6 +1,6 @@
 --! Previous: sha1:e93162fdb23913e9ef6913acaf4eac5870fa30fe
---! Hash: sha1:6c8b81111d1ea84d1e1fbda553e11e2b057dcf2a
+--! Hash: sha1:4d0fbb44b86f182fc71d9dc1e8e6a4c0d8a4391e
 
 -- Enter migration here
 ALTER TABLE campaigns DROP COLUMN IF EXISTS texts CASCADE;
-ALTER TABLE campaigns ADD COLUMN texts BOOLEAN;
+ALTER TABLE campaigns ADD COLUMN texts JSONB NOT NULL;
