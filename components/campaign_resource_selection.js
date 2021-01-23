e70f1d7108afb3f18f2b58c24d00dc4ecf966c88
Sascha Mayr <mayr.sascha@gmail.com>
2021-01-23 14:30:39 +0100

    Improve resource selection

--- components/campaign_resource_selection.js
+++ components/campaign_resource_selection.js
@@ -1,10 +1,15 @@
 import { useState } from 'react';
 import { ResourcePicker } from '@shopify/app-bridge-react';
-import { Button, ResourceList, ResourceItem, TextStyle, TextField, Select, Icon } from '@shopify/polaris';
 import {
-  MobileCancelMajor,
-  ImageMajor
-} from '@shopify/polaris-icons';
+  Button,
+  ResourceList,
+  ResourceItem,
+  TextStyle,
+  TextField,
+  Select,
+  Icon,
+} from '@shopify/polaris';
+import { MobileCancelMajor, ImageMajor } from '@shopify/polaris-icons';
 import { ApolloConsumer, useQuery } from 'react-apollo';
 import { gql } from 'apollo-boost';
 import '../styles/components_resource_selection.css';
@@ -58,131 +63,159 @@ const CampaignResourceSelection = ({
   buttonProps,
   resources = [],
   onResourceMutation,
-  applyDiscount
+  applyDiscount,
 }) => {
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
   const { label } = buttonProps;
   const { data } = useQuery(GET_STORE_CURRENCY);
-  const currencyCode = data && data.shop ? data.shop.currencyCode : 'USD';
+  const currencyCode = data && data.shop && data.shop.currencyCode;
   return (
-  <>
-    <div className='salestorm-resources'>
-      <ResourceList
-        items={resources}
-        renderItem={(resource) => {
-          const { title, id, images, discount } = resource;
-          return (
-            <ResourceItem
-              id={id}
-              accessibilityLabel={`View details for ${title}`}
-            >
-              <div className='salestorm-resource-item'>
-                <div className='salestorm-resource-item-info'>
-                  {
-                    images.edges.length > 0 ?
-                    <img src={images.edges[0].node.transformedSrc} alt={images.edges[0].node.altText}/>
-                    :
-                    <div className='salestorm-resource-image-placeholder'>
-                      <Icon source={ImageMajor} />
-                    </div>
-                  }
-                  <TextStyle variation="strong">{title}</TextStyle>
+    <>
+      <div className="salestorm-resources">
+        <ResourceList
+          items={resources}
+          renderItem={(resource) => {
+            const { title, id, images, discount } = resource;
+            let thumbnail;
+            if (images.edges.length > 0) {
+              thumbnail = (
+                <img
+                  src={images.edges[0].node.transformedSrc}
+                  alt={images.edges[0].node.altText}
+                />
+              );
+            } else {
+              thumbnail = (
+                <div className="salestorm-resource-image-placeholder">
+                  <Icon source={ImageMajor} />
                 </div>
-                <div className='salestorm-resource-actions'>
-                  {
-                    applyDiscount &&
-                    <div className='salestorm-resource-discount'>
-                      <TextField
-                        value={`${discount.value}`}
-                        onChange={(value) => {
-                          let helperResources = resources.map(resource => {
-                            if (resource.id === id) {
-                              resource.discount.value = value;
-                            }
-                            return resource;
-                          });
-                          onResourceMutation(helperResources);
-                        }}
-                        placeholder='Discount'
-                      />
-                      <Select
-                        options={[
-                          { label: currencyCode, value: currencyCode },
-                          { label: '%', value: '%'}
-                        ]}
-                        onChange={(value) => {
-                          let helperResources = resources.map(resource => {
-                            if (resource.id === id) {
-                              resource.discount.type = value;
-                            }
-                            return resource;
-                          });
-                          onResourceMutation(helperResources);
-                        }}
-                        value={discount.type}
-                      />
+              );
+            }
+            return (
+              <ResourceItem
+                id={id}
+                accessibilityLabel={`View details for ${title}`}
+              >
+                <div className="salestorm-resource-item">
+                  <div className="salestorm-resource-item-info">
+                    {thumbnail}
+                    <TextStyle variation="strong">{title}</TextStyle>
+                  </div>
+                  <div className="salestorm-resource-actions">
+                    {applyDiscount && (
+                      <div className="salestorm-resource-discount">
+                        <TextField
+                          value={`${discount.value}`}
+                          onChange={(value) => {
+                            const helperResources = resources.map(
+                              (helperResource) => {
+                                if (helperResource.id === id) {
+                                  helperResource.discount.value = value;
+                                }
+                                return helperResource;
+                              }
+                            );
+                            onResourceMutation(helperResources);
+                          }}
+                          placeholder="Discount"
+                        />
+                        <Select
+                          options={[
+                            { label: currencyCode, value: currencyCode },
+                            { label: '%', value: '%' },
+                          ]}
+                          onChange={(value) => {
+                            const helperResources = resources.map(
+                              (helperResource) => {
+                                if (helperResource.id === id) {
+                                  helperResource.discount.type = value;
+                                }
+                                return helperResource;
+                              }
+                            );
+                            onResourceMutation(helperResources);
+                          }}
+                          value={discount.type}
+                        />
+                      </div>
+                    )}
+                    <div
+                      className="salestorm-resource-remove"
+                      onClick={() => {
+                        onResourceMutation(
+                          resources.filter(
+                            (findResource) => findResource.id !== id
+                          )
+                        );
+                      }}
+                    >
+                      <Icon source={MobileCancelMajor} />
                     </div>
-                  }
-                  <div className='salestorm-resource-remove' onClick={() => {
-                    onResourceMutation(resources.filter(findResource => findResource.id !== id))
-                  }}>
-                    <Icon source={MobileCancelMajor} />
                   </div>
                 </div>
-              </div>
-            </ResourceItem>
-          )
-        }}
-      />
-    </div>
+              </ResourceItem>
+            );
+          }}
+        />
+      </div>
       <ApolloConsumer>
-        {
-          client => (
-            <ResourcePicker
-              {...resourcePickerProps}
-              open={open}
-              onCancel={() => setOpen(false)}
-              onSelection={async (selectPayload) => {
-                setOpen(false);
-                if (resourcePickerProps.resourceType === 'Product') {
-                  setLoading(true);
-                  const products = await Promise.all(selectPayload.selection.map(async (resource) => {
+        {(client) => (
+          <ResourcePicker
+            {...resourcePickerProps}
+            open={open}
+            onCancel={() => setOpen(false)}
+            onSelection={async (selectPayload) => {
+              setOpen(false);
+              if (resourcePickerProps.resourceType === 'Product') {
+                setLoading(true);
+                const products = await Promise.all(
+                  selectPayload.selection.map(async (resource) => {
                     const { id, title } = resource;
                     try {
                       const product = await client.query({
                         query: GET_PRODUCT,
                         variables: {
-                          id
-                      }});
-                      const product_data = product.data.product;
-                      return applyDiscount ? {...product_data, discount: { type: currencyCode, value: 5 }} : product_data;
+                          id,
+                        },
+                      });
+                      const productData = product.data.product;
+                      return applyDiscount
+                        ? {
+                            ...productData,
+                            discount: { type: currencyCode, value: 5 },
+                          }
+                        : productData;
                     } catch (error) {
-                      console.log(`Failed to load product data for product: ${title}`);
+                      console.log(
+                        `Failed to load product data for product: ${title}`
+                      );
                     }
-                  }));
-                  setLoading(false);
-                  onResourceMutation(products);
-                }
-                else {
-                  onResourceMutation(selectPayload.selection);
-                }
-              }}
-            />
-          )
-        }
+                  })
+                );
+                setLoading(false);
+                onResourceMutation(products);
+              } else {
+                onResourceMutation(selectPayload.selection);
+              }
+            }}
+          />
+        )}
       </ApolloConsumer>
-    {
-      resourcePickerProps.selectMultiple || resources.length === 0 ?
-      <div className='salestorm-add-resource-button-container'>
-        <Button {...buttonProps} onClick={() => setOpen(true)} loading={loading}>
-          {label}
-        </Button>
-      </div>
-      : <></>
-    }
-  </>
-  )
-}
+      {resourcePickerProps.selectMultiple ||
+        (resources.length === 0 && (
+          <div className="salestorm-add-resource-button-container">
+            <Button
+              {...buttonProps}
+              onClick={() => setOpen(true)}
+              loading={loading}
+            >
+              {label}
+            </Button>
+          </div>
+        ))}
+    </>
+  );
+};
 
 export default CampaignResourceSelection;
