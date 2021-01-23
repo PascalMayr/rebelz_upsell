f15510e42375929da7aa76c893a4db836d0a88e0
Sascha Mayr <mayr.sascha@gmail.com>
2021-01-23 14:16:34 +0100

    Fix placeholder ESLint

--- components/campaign_preview_placeholder.js
+++ components/campaign_preview_placeholder.js
@@ -1,16 +1,18 @@
 const CampaignPreviewPlaceholder = {
-  legacyResourceId: "6169782747321",
-  title:"The Mini Handbag by rothys",
-  descriptionHtml: "A small structured staple. Crafted with ocean-bound marine plastic, this bag features a top handle and removable strap—so you can create the style you need for any occasion.",
+  legacyResourceId: '6169782747321',
+  title: 'The Mini Handbag by rothys',
+  descriptionHtml:
+    'A small structured staple. Crafted with ocean-bound marine plastic, this bag features a top handle and removable strap—so you can create the style you need for any occasion.',
   images: {
-    edges:[
+    edges: [
       {
-        node:{
-          transformedSrc: 'https://cdn.shopify.com/s/files/1/0877/4986/products/032_Candy_Apple_pdp_A.jpg?v=1588623182&width=500',
-          altText: null
-        }
-      }
-    ]
+        node: {
+          transformedSrc:
+            'https://cdn.shopify.com/s/files/1/0877/4986/products/032_Candy_Apple_pdp_A.jpg?v=1588623182&width=500',
+          altText: null,
+        },
+      },
+    ],
   },
   variants: {
     edges: [
@@ -18,10 +20,11 @@ const CampaignPreviewPlaceholder = {
         node: {
           selectedOptions: [{ name: 'Color' }],
           title: 'Red',
-          legacyResourceId:"38752185811129",
+          legacyResourceId: '38752185811129',
           image: {
-            transformedSrc: 'https://cdn.shopify.com/s/files/1/0877/4986/products/032_Candy_Apple_pdp_A.jpg?v=1588623182&width=500',
-            altText: null
+            transformedSrc:
+              'https://cdn.shopify.com/s/files/1/0877/4986/products/032_Candy_Apple_pdp_A.jpg?v=1588623182&width=500',
+            altText: null,
           },
         },
       },
@@ -29,19 +32,20 @@ const CampaignPreviewPlaceholder = {
         node: {
           selectedOptions: [{ name: 'Color' }],
           title: 'Blue',
-          legacyResourceId:"38752185811121",
+          legacyResourceId: '38752185811121',
           image: {
-            transformedSrc: 'https://cdn.shopify.com/s/files/1/0877/4986/products/032_Royal_Blue_pdp_A.jpg?v=1588713960&width=500',
-            altText: null
+            transformedSrc:
+              'https://cdn.shopify.com/s/files/1/0877/4986/products/032_Royal_Blue_pdp_A.jpg?v=1588713960&width=500',
+            altText: null,
           },
         },
       },
-    ]
+    ],
   },
   discount: {
-    type: "%",
+    type: '%',
     value: 5,
   },
-}
+};
 
-export default CampaignPreviewPlaceholder
+export default CampaignPreviewPlaceholder;
