b8506bb40a39ea20d71f0ce0c17ff657fb6df0b9
Pascal Mayr <pascalmayr2@gmail.com>
2021-02-03 03:36:11 +0100

    adding some more popup JS to the webcomponent

--- components/popup/set_web_component_dom_definition.js
+++ components/popup/set_web_component_dom_definition.js
@@ -9,7 +9,12 @@ const setWebComponentDomDefinitionPopup = (templateID) => `
     }
 
     connectedCallback() {
-      this.setupClickListener();
+      window.Salestorm = {
+        popupId: "${templateID}",
+        hidePopup: new Event('salestorm-hide-popup-event'),
+      };
+      this.setupClickListeners();
+      this.setupKeyListeners();
     }
 
     disconnectedCallback() {
@@ -29,18 +34,41 @@ const setWebComponentDomDefinitionPopup = (templateID) => `
 
     addCssDynamic() {}
 
-    setupClickListener() {
-      const closeButton = document.querySelector('#salestorm-popup-close');
-      console.log(closeButton)
+    setupClickListeners() {
+      const productDetailsMessage = this.shadow.querySelector('#salestorm-campaign-text-seeProductDetailsAction');
+      const descriptionElement = this.shadow.querySelector('#salestorm-product-description');
+      if (descriptionElement && productDetailsMessage) {
+        descriptionElement.style.display = 'none';
+        productDetailsMessage.addEventListener('click', () => {
+          descriptionElement.style.display = descriptionElement.style.display === 'block' ? 'none' : 'block';
+        });
+      }
+
+      const closeButton = this.shadow.querySelector('#salestorm-popup-close');
       closeButton && closeButton.addEventListener('click', () => {
         this.hidePopup();
       });
-      this.hidePopup();
+      const dismissAction = this.shadow.querySelector('#salestorm-campaign-text-dismissAction');
+      dismissAction && dismissAction.addEventListener('click', () => {
+        this.hidePopup();
+      });
+      const checkoutAction = this.shadow.querySelector('#salestorm-popup-footer-checkout-action');
+      checkoutAction && checkoutAction.addEventListener('click', () => {
+        this.hidePopup();
+      });
+    }
+
+    setupKeyListeners() {
+      document.addEventListener("keydown", event => {
+        if(event.key === "Escape") {
+          this.hidePopup();
+        }
+      });
     }
 
     hidePopup() {
-      console.log('hey')
-      //document.querySelector('#salestorm-overlay-container').style.display = 'none';
+      document.dispatchEvent(window.Salestorm.hidePopup);
+      this.shadow.querySelector('#salestorm-overlay-container').style.display = 'none';
     }
   }
   if (!customElements.get('salestorm-popup')) {
