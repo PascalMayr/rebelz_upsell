a76133126a345c6d51d970c67e9d090eafbd7cc9
Pascal Mayr <pascalmayr2@gmail.com>
2021-02-07 01:07:53 +0100

    updated the custom element definition

--- components/popup/define_custom_element.js
+++ components/popup/define_custom_element.js
@@ -1,25 +1,26 @@
-const defineCustomElementPopup = (templateID) => `
+const defineCustomElementPopup = (popupID) => `
   class SalestormPopupComponent extends HTMLElement {
     shadow;
+    renderedProduct = 'test';
+
+    constructor() {
+      super();
+      this.setupShadow();
+    }
 
     static get observedAttributes() {
       return ['visible'];
     }
 
-    constructor() {
-      super();
-
-      this.setupShadow();
-    }
-
     connectedCallback() {
       window.Salestorm = {
-        popupId: "${templateID}",
+        popupId: "${popupID}",
         hidePopup: new Event('salestorm-hide-popup-event'),
         hideProductDetails: new Event('salestorm-hide-product-details'),
       };
       this.setupClickListeners();
       this.setupKeyListeners();
+      this.setupHidePopupListener();
     }
 
     disconnectedCallback() {
@@ -41,10 +42,7 @@ const defineCustomElementPopup = (templateID) => `
 
     setupShadow() {
       this.shadow = this.attachShadow({ mode: 'open' });
-      const template = document.getElementById("${templateID}");
-      if (template) {
-        this.shadow.appendChild(template.content.cloneNode(true));
-      }
+      this.shadow.innerHTML = this.innerHTML;
     }
 
     setupClickListeners() {
@@ -80,45 +78,14 @@ const defineCustomElementPopup = (templateID) => `
       });
     }
 
-    hidePopup() {
-      document.dispatchEvent(window.Salestorm.hidePopup);
-      this.shadow.querySelector('#salestorm-overlay-container').style.display = 'none';
+    setupHidePopupListener() {
+      document.addEventListener(window.Salestorm.hidePopup.type, () => {
+        this.shadow.querySelector('#salestorm-overlay-container').style.display = 'none';
+      })
     }
 
-    findDisplayCurrencyCode() {
-      if (window.afterpay_shop_currency && window.afterpay_shop_currency !== "") {
-        return window.afterpay_shop_currency;
-      }
-      if (window.shop_currency && window.shop_currency !== "") {
-        return window.shop_currency;
-      }
-      if (window.mlvedaShopCurrency && window.mlvedaShopCurrency !== "") {
-        return window.mlvedaShopCurrency;
-      }
-      if (window.Currency && window.Currency.currentCurrency && window.Currency.currentCurrency !== "") {
-        return window.Currency.currentCurrency;
-      }
-      if (window.Currency && window.Currency.shop_currency && window.Currency.shop_currency !== "") {
-        return window.Currency.shop_currency;
-      }
-      if (window.localStorage) {
-        if (localStorage.getItem('currency') && localStorage.getItem('currency') !== "") {
-          return localStorage.getItem('currency');
-        }
-        if (localStorage.getItem('GIP_USER_CURRENCY') && localStorage.getItem('GIP_USER_CURRENCY') !== "") {
-          return localStorage.getItem('GIP_USER_CURRENCY');
-        }
-        if (localStorage.getItem('currencyWidget') && localStorage.getItem('currencyWidget') !== "") {
-          return localStorage.getItem('currencyWidget');
-        }
-      }
-      if (document.getElementById('baCurrSelector')) {
-        return document.getElementById('baCurrSelector').value;
-      }
-      if (window.Shopify && window.Shopify.currency && window.Shopify.currency.active !== "") {
-        return Shopify.currency.active;
-      }
-      return null;
+    hidePopup() {
+      document.dispatchEvent(window.Salestorm.hidePopup);
     }
 
   }
