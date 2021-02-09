55a21a2ec5239db3e91609b3edfefd056702ec55
Pascal Mayr <pascalmayr2@gmail.com>
2021-02-09 23:53:32 +0100

    converting prices correctly and removing dirty useEffect hooks

--- components/popup/preview.js
+++ components/popup/preview.js
@@ -2,7 +2,6 @@ import React, { useEffect, useRef } from 'react';
 
 import getAnimationClassPopup from './get_animation_class';
 import getRenderedProductPopup from './get_rendered_product';
-import processCampaignTextsPopup from './process_campaign_texts';
 import TemplateLoader from './templates';
 
 const PreviewPopup = ({ campaign, preview }) => {
@@ -27,45 +26,13 @@ const PreviewPopup = ({ campaign, preview }) => {
     }
   };
 
-  useEffect(() => {
-    if (webComponentRefShadow) {
-      Object.keys(campaign.texts).forEach((textKey) => {
-        const campaignTextElement = webComponentRefShadow.querySelector(
-          `#salestorm-campaign-text-${textKey}`
-        );
-        if (
-          campaignTextElement &&
-          campaignTextElement.innerHTML !== campaign.texts[textKey]
-        ) {
-          campaignTextElement.innerHTML = processCampaignTextsPopup(
-            campaign.texts[textKey]
-          );
-        }
-      });
-    }
-  }, [campaign.texts, webComponentRefShadow]);
-
-  useEffect(() => {
-    if (webComponentRefShadow) {
-      const popupContainerElement = webComponentRefShadow.querySelector(
-        '#salestorm-popup'
-      );
-      if (popupContainerElement) {
-        popupContainerElement.className = getAnimationClassPopup(
-          campaign.animation
-        );
-      }
-    } else {
-      return null;
-    }
-  }, [campaign.animation, webComponentRefShadow]);
-
-  const renderedProduct = getRenderedProductPopup(campaign);
-
   return (
     <salestorm-popup
       ref={webComponentRef}
-      product={JSON.stringify(renderedProduct)}
+      product={JSON.stringify(getRenderedProductPopup(campaign))}
+      texts={JSON.stringify(campaign.texts)}
+      multicurrency={campaign.multiCurrencySupport}
+      animation={getAnimationClassPopup(campaign.animation)}
     >
       <TemplateLoader
         campaign={campaign}
--- components/popup/templates/debut/define_custom_popup_element.js
+++ components/popup/templates/debut/define_custom_popup_element.js
@@ -1,6 +1,7 @@
+import processCampaignTextsPopup from '../../process_campaign_texts';
+
 const defineCustomPopupElementDebut = `
   class SalestormPopupComponent extends HTMLElement {
-    shadow;
 
     constructor() {
       super();
@@ -8,7 +9,27 @@ const defineCustomPopupElementDebut = `
     }
 
     static get observedAttributes() {
-      return ['visible', 'product'];
+      return ['visible', 'product', 'multicurrency', 'texts', 'animation'];
+    }
+
+    attributeChangedCallback(name, _oldValue, newValue) {
+      if (name === "visible") {
+        if (newValue === "true") {
+          this.shadowRoot.querySelector('#salestorm-overlay-container').style.display = 'flex';
+        }
+        else {
+          this.hidePopup();
+        }
+      }
+      if (name === 'product') {
+        this.updateProduct(JSON.parse(newValue));
+      }
+      if (name === 'texts') {
+        this.updateTexts(JSON.parse(newValue));
+      }
+      if (name === 'animation') {
+        this.updateAnimation(newValue);
+      }
     }
 
     connectedCallback() {
@@ -21,51 +42,84 @@ const defineCustomPopupElementDebut = `
     }
 
     disconnectedCallback() {
-      // remove events
-    }
-
-    adoptedCallback() {}
-
-    attributeChangedCallback(name, _oldValue, newValue) {
-      if (name === "visible") {
-        if (newValue === "true") {
-          this.shadow.querySelector('#salestorm-overlay-container').style.display = 'flex';
-        }
-        else {
-          this.hidePopup();
-        }
-      }
-      if (name === 'product') {
-        this.updateProduct(JSON.parse(newValue));
-      }
+      this.removeClickListeners();
+      this.removeKeyListeners();
+      this.removeHidePopupListener();
     }
 
     setupShadow() {
-      this.shadow = this.attachShadow({ mode: 'open' });
-      this.shadow.innerHTML = this.innerHTML;
+      this.attachShadow({ mode: 'open' });
+      this.shadowRoot.innerHTML = this.innerHTML;
+    }
+
+    toggleDescriptionElement(element) {
+      element.classList.toggle('d-none');
+    }
+
+    getDetailsMessageElement() {
+      return this.shadowRoot.querySelector('#salestorm-campaign-text-seeProductDetailsAction');
+    }
+
+    getCloseButtonElement() {
+      return this.shadowRoot.querySelector('#salestorm-popup-close');
+    }
+
+    getClaimOfferButtonElement() {
+      return this.shadowRoot.querySelector('#salestorm-claim-offer-button');
+    }
+
+    getProductDescriptionElement() {
+      return this.shadowRoot.querySelector('#salestorm-product-description');
+    }
+
+    getDismissActionElement() {
+      return this.shadowRoot.querySelector('#salestorm-campaign-text-dismissAction');
+    }
+
+    getCheckoutElement() {
+      return this.shadowRoot.querySelector('#salestorm-popup-footer-checkout-action');
+    }
+
+    getSelectElements() {
+      return this.shadowRoot.querySelectorAll('.salestorm-product-select');
+    }
+
+    getAddToCartActionElement() {
+      return this.shadowRoot.querySelector('#salestorm-campaign-text-addToCartAction');
+    }
+
+    getAddToCartUnavailableVariationElement() {
+      return this.shadowRoot.querySelector('#salestorm-campaign-text-addToCartUnavailableVariation');
     }
 
     setupClickListeners() {
-      const productDetailsMessage = this.shadow.querySelector('#salestorm-campaign-text-seeProductDetailsAction');
-      const descriptionElement = this.shadow.querySelector('#salestorm-product-description');
+      const productDetailsMessage = this.getDetailsMessageElement();
+      const descriptionElement = this.getProductDescriptionElement();
       if (descriptionElement && productDetailsMessage) {
-        productDetailsMessage.addEventListener('click', () => {
-          descriptionElement.classList.toggle('d-none');
-        });
+        productDetailsMessage.addEventListener('click', () => this.toggleDescriptionElement(descriptionElement));
       }
 
-      const closeButton = this.shadow.querySelector('#salestorm-popup-close');
-      closeButton && closeButton.addEventListener('click', () => {
-        this.hidePopup();
-      });
-      const dismissAction = this.shadow.querySelector('#salestorm-campaign-text-dismissAction');
-      dismissAction && dismissAction.addEventListener('click', () => {
-        this.hidePopup();
-      });
-      const checkoutAction = this.shadow.querySelector('#salestorm-popup-footer-checkout-action');
-      checkoutAction && checkoutAction.addEventListener('click', () => {
-        this.hidePopup();
-      });
+      const closeButton = this.getCloseButtonElement();
+      closeButton && closeButton.addEventListener('click', this.hidePopup);
+      const dismissAction = this.getDismissActionElement();
+      dismissAction && dismissAction.addEventListener('click', this.hidePopup);
+      const checkoutAction = this.getCheckoutElement();
+      checkoutAction && checkoutAction.addEventListener('click', this.hidePopup);
+    }
+
+    removeClickListeners() {
+      const productDetailsMessage = this.getDetailsMessageElement();
+      const descriptionElement = this.getProductDescriptionElement();
+      if (descriptionElement && productDetailsMessage) {
+        productDetailsMessage.removeEventListener('click', this.toggleDescriptionElement);
+      }
+
+      const closeButton = this.getCloseButtonElement();
+      closeButton && closeButton.removeEventListener('click', this.hidePopup);
+      const dismissAction = this.getDismissActionElement();
+      dismissAction && dismissAction.removeEventListener('click', this.hidePopup);
+      const checkoutAction = this.getCheckoutElement();
+      checkoutAction && checkoutAction.removeEventListener('click', this.hidePopup);
     }
 
     setupKeyListeners() {
@@ -76,9 +130,23 @@ const defineCustomPopupElementDebut = `
       });
     }
 
+    removeKeyListeners() {
+      document.removeEventListener("keydown", event => {
+        if(event.key === "Escape") {
+          this.hidePopup();
+        }
+      });
+    }
+
     setupHidePopupListener() {
       document.addEventListener(window.Salestorm.hidePopup.type, () => {
-        this.shadow.querySelector('#salestorm-overlay-container').style.display = 'none';
+        this.shadowRoot.querySelector('#salestorm-overlay-container').style.display = 'none';
+      })
+    }
+
+    removeHidePopupListener() {
+      document.removeEventListener(window.Salestorm.hidePopup.type, () => {
+        this.shadowRoot.querySelector('#salestorm-overlay-container').style.display = 'none';
       })
     }
 
@@ -86,31 +154,100 @@ const defineCustomPopupElementDebut = `
       document.dispatchEvent(window.Salestorm.hidePopup);
     }
 
+
     setSelectedProductVariant(product) {
       const sortStringArrayAlphabetically = array => array.sort((a, b) => a.length - b.length);
-      const currentRenderedSelects = Array.from(this.shadow.querySelectorAll('.salestorm-product-select'));
+      const currentRenderedSelects = Array.from(this.getSelectElements());
       const currentSelectionState = currentRenderedSelects.map(selectElement => selectElement.value);
       const selectedVariant = product.variants.edges.find(variant => {
         const variantOptionValues = variant.node.selectedOptions.map(selectedOption => selectedOption.value);
         return JSON.stringify(sortStringArrayAlphabetically(variantOptionValues)) === JSON.stringify(sortStringArrayAlphabetically(currentSelectionState));
       });
 
-      const claimOfferButton = this.shadow.querySelector('#salestorm-claim-offer-button');
+      const claimOfferButton = this.getClaimOfferButtonElement();
       if (selectedVariant) {
         if (selectedVariant.node && selectedVariant.node.image) {
-          this.shadow.querySelector('#salestorm-product-image').style.backgroundImage = "url("+selectedVariant.node.image.transformedSrc+")";
+          this.shadowRoot.querySelector('#salestorm-product-image').style.backgroundImage = "url("+selectedVariant.node.image.transformedSrc+")";
         }
         claimOfferButton.classList.disable = false;
         claimOfferButton.classList.remove('offer-button-disabled');
-        this.shadow.querySelector('#salestorm-campaign-text-addToCartAction').classList.remove('d-none');
-        this.shadow.querySelector('#salestorm-campaign-text-addToCartUnavailableVariation').classList.add('d-none');
+        this.getAddToCartActionElement().classList.remove('d-none');
+        this.getAddToCartUnavailableVariationElement().classList.add('d-none');
       }
       else {
         if (currentRenderedSelects.length > 0) {
           claimOfferButton.classList.disable = true;
           claimOfferButton.classList.add('offer-button-disabled');
-          this.shadow.querySelector('#salestorm-campaign-text-addToCartAction').classList.add('d-none');
-          this.shadow.querySelector('#salestorm-campaign-text-addToCartUnavailableVariation').classList.remove('d-none');
+          this.getAddToCartActionElement().classList.add('d-none');
+          this.getAddToCartUnavailableVariationElement().classList.remove('d-none');
+        }
+      }
+    }
+
+    getDisplayedStoreCurrencyCode() {
+      if (window.afterpay_shop_currency && window.afterpay_shop_currency !== "") {
+        return window.afterpay_shop_currency;
+      }
+      if (window.shop_currency && window.shop_currency !== "") {
+        return window.shop_currency;
+      }
+      if (window.mlvedaShopCurrency && window.mlvedaShopCurrency !== "") {
+        return window.mlvedaShopCurrency;
+      }
+      if (window.Currency && window.Currency.currentCurrency && window.Currency.currentCurrency !== "") {
+        return window.Currency.currentCurrency;
+      }
+      if (window.Currency && window.Currency.shop_currency && window.Currency.shop_currency !== "") {
+        return window.Currency.shop_currency;
+      }
+      if (window.localStorage) {
+        if (localStorage.getItem('currency') && localStorage.getItem('currency') !== "") {
+          return localStorage.getItem('currency');
+        }
+        if (localStorage.getItem('GIP_USER_CURRENCY') && localStorage.getItem('GIP_USER_CURRENCY') !== "") {
+          return localStorage.getItem('GIP_USER_CURRENCY');
+        }
+        if (localStorage.getItem('currencyWidget') && localStorage.getItem('currencyWidget') !== "") {
+          return localStorage.getItem('currencyWidget');
+        }
+      }
+      if (document.getElementById('baCurrSelector')) {
+        return document.getElementById('baCurrSelector').value;
+      }
+      if (window.Shopify && window.Shopify.currency && window.Shopify.currency.active !== "") {
+        return Shopify.currency.active;
+      }
+      return null;
+    }
+
+    updatePrices(product) {
+      if (Boolean(product.discount)) {
+        const salestormPrices = this.shadowRoot.querySelectorAll('.salestorm-price');
+        const multiCurrencySupport = this.getAttribute("multicurrency") === "true";
+        if (product.discount.type !== "%") {
+          const baseCurrencyCode = product.discount.type;
+          window.Salestorm.currentCurrencyCode = (multiCurrencySupport && this.getDisplayedStoreCurrencyCode())|| baseCurrencyCode;
+
+          const currencyFormatter = new Intl.NumberFormat([], {
+            style: 'currency',
+            currency: window.Salestorm.currentCurrencyCode,
+            currencyDisplay: 'symbol',
+            maximumSignificantDigits: 3
+          });
+
+          salestormPrices.forEach(priceElement => {
+            const priceValue = product.discount.value || 0;
+            let convertedPriceValue = priceValue;
+            if (window.Currency && window.Currency.rates && window.Currency.convert && multiCurrencySupport) {
+              convertedPriceValue = Math.round(window.Currency.convert(priceValue, baseCurrencyCode, window.Salestorm.currentCurrencyCode));
+            }
+            priceElement.innerText = currencyFormatter.format(convertedPriceValue);
+          });
+        }
+        else {
+          salestormPrices.forEach(priceElement => {
+            priceElement.innerText = product.discount.value + "%";
+          });
         }
       }
     }
@@ -119,7 +256,7 @@ const defineCustomPopupElementDebut = `
       const elementUpdateSelectors = ['#salestorm-product-title', '#salestorm-product-variants', '#salestorm-product-description'];
       for (let elementUpdateSelector of elementUpdateSelectors) {
         const updatedNode = document.querySelector(elementUpdateSelector);
-        const oldNode = this.shadow.querySelector(elementUpdateSelector);
+        const oldNode = this.shadowRoot.querySelector(elementUpdateSelector);
         if (updatedNode && oldNode) {
           oldNode.replaceWith(updatedNode);
         }
@@ -128,17 +265,17 @@ const defineCustomPopupElementDebut = `
       if (product) {
         const mainProductImage = product.images.edges.length > 0 && product.images.edges[0].node.transformedSrc;
         if (mainProductImage) {
-          this.shadow.querySelector('#salestorm-product-image').style.backgroundImage = "url("+mainProductImage+")";
+          this.shadowRoot.querySelector('#salestorm-product-image').style.backgroundImage = "url("+mainProductImage+")";
         } else {
-          this.shadow.querySelector('#salestorm-product-image').style.backgroundImage = "";
+          this.shadowRoot.querySelector('#salestorm-product-image').style.backgroundImage = "";
         }
         if (product.descriptionHtml === '') {
-          this.shadow.querySelector('#salestorm-campaign-text-seeProductDetailsAction').style.display = 'none';
+          this.shadowRoot.querySelector('#salestorm-campaign-text-seeProductDetailsAction').style.display = 'none';
         } else {
-          this.shadow.querySelector('#salestorm-campaign-text-seeProductDetailsAction').style.display = 'block';
+          this.shadowRoot.querySelector('#salestorm-campaign-text-seeProductDetailsAction').style.display = 'block';
         }
         this.setSelectedProductVariant(product);
-        this.shadow.querySelectorAll('.salestorm-product-select').forEach(selectElement => {
+        this.shadowRoot.querySelectorAll('.salestorm-product-select').forEach(selectElement => {
           selectElement.addEventListener('change', () => {
             const selectedValue = selectElement.value;
             this.setSelectedProductVariant(product);
@@ -146,6 +283,31 @@ const defineCustomPopupElementDebut = `
         });
       }
 
+      this.updatePrices(product);
+
+    }
+
+    updateTexts(texts) {
+      ${processCampaignTextsPopup.toString()}
+      Object.keys(texts).forEach((textKey) => {
+        const campaignTextElement = this.shadowRoot.querySelector(
+          "#salestorm-campaign-text-" + textKey
+        );
+        if (
+          campaignTextElement &&
+          campaignTextElement.innerHTML !== texts[textKey]
+        ) {
+          campaignTextElement.innerHTML = processCampaignTextsPopup(
+            texts[textKey]
+          );
+        }
+      });
+      const product = this.getAttribute('product');
+      this.updatePrices(JSON.parse(product));
+    }
+
+    updateAnimation(animation) {
+      this.shadowRoot.querySelector('#salestorm-popup').className = animation;
     }
 
   }
