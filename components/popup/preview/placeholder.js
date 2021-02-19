f536e028f5c53890cf94ef7da8bc73c6968f4897
Pascal Mayr <pascalmayr2@gmail.com>
2021-02-19 04:08:25 +0100

    updating the placeholder

--- components/popup/preview/placeholder.js
+++ components/popup/preview/placeholder.js
@@ -3,16 +3,10 @@ const PlaceholderPreview = {
   title: 'The Mini Handbag by rothys',
   descriptionHtml:
     'A small structured staple. Crafted with ocean-bound marine plastic, this bag features a top handle and removable strap—so you can create the style you need for any occasion.',
-  images: {
-    edges: [
-      {
-        node: {
-          transformedSrc:
-            'https://cdn.shopify.com/s/files/1/0877/4986/products/032_Candy_Apple_pdp_A.jpg?v=1588623182&width=500',
-          altText: null,
-        },
-      },
-    ],
+  featuredImage: {
+    transformedSrc:
+    'https://cdn.shopify.com/s/files/1/0877/4986/products/032_Candy_Apple_pdp_A.jpg?v=1588623182&width=500',
+    altText: null,
   },
   options:[
     {
@@ -20,6 +14,10 @@ const PlaceholderPreview = {
       values: ['Red', 'Blue'],
     },
   ],
+  hasOnlyDefaultVariant: false,
+  totalVariants: 2,
+  status: 'ACTIVE',
+  handle: 'young-frog',
   variants: {
     edges: [
       {
@@ -27,6 +25,7 @@ const PlaceholderPreview = {
           selectedOptions: [{ name: 'Color', value: 'Red' }],
           title: 'Red',
           legacyResourceId: '38752185811129',
+          availableForSale: true,
           image: {
             transformedSrc:
               'https://cdn.shopify.com/s/files/1/0877/4986/products/032_Candy_Apple_pdp_A.jpg?v=1588623182&width=500',
@@ -39,6 +38,7 @@ const PlaceholderPreview = {
           selectedOptions: [{ name: 'Color', value: 'Blue' }],
           title: 'Blue',
           legacyResourceId: '38752185811121',
+          availableForSale: false,
           image: {
             transformedSrc:
               'https://cdn.shopify.com/s/files/1/0877/4986/products/032_Royal_Blue_pdp_A.jpg?v=1588713960&width=500',
