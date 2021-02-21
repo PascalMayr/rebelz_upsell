877b2fbdb6444ad268e53d47baaf78e2f520f484
Pascal Mayr <pascalmayr2@gmail.com>
2021-02-21 04:32:14 +0100

    passing the current strategy to the preview placeholder

--- components/popup/utils/get_rendered_product.js
+++ components/popup/utils/get_rendered_product.js
@@ -3,6 +3,6 @@ import PlaceholderPreview from '../preview/placeholder';
 const getRenderedProductUtil = (campaign) =>
   campaign.selling.products.length > 0
     ? campaign.selling.products[0]
-    : PlaceholderPreview;
+    : PlaceholderPreview(campaign.strategy);
 
 export default getRenderedProductUtil;
