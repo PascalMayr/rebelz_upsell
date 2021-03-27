dfc08bb337291f5ff764928857e6e0cf52f1aa63
Pascal Mayr <pascalmayr2@gmail.com>
2021-03-27 23:42:30 +0100

    exporting create draft order

---
+++ server/controllers/create_draft_order.js
@@ -0,0 +1,134 @@
+/* eslint-disable babel/camelcase */
+import db from '../db';
+
+import reportError from './report_error';
+
+const createDraftOrder = async (ctx) => {
+  try {
+    const {
+      variantId,
+      strategy,
+      quantity,
+      cart,
+      shop,
+      id,
+      products,
+    } = ctx.request.body;
+    const store = await db.query(
+      `SELECT * FROM stores WHERE stores.domain = $1`,
+      [shop]
+    );
+    const accessToken = store.rows[0].access_token;
+    const draftOrder = {
+      line_items: cart.items.map((item) => ({ ...item, properties: [] })),
+    };
+    const { sellType, mode } = strategy;
+    if (mode === 'discount') {
+      const campaignItem = {
+        variant_id: variantId,
+        quantity,
+        applied_discount: {
+          value: strategy.discount.value,
+          value_type:
+            strategy.discount.type === '%' ? 'percentage' : 'fixed_amount',
+        },
+      };
+      if (sellType === 'upsell') {
+        draftOrder.line_items = draftOrder.line_items.filter(
+          (lineItem) =>
+            !products.find(
+              (product) =>
+                product.legacyResourceId.toString() ===
+                lineItem.product_id.toString()
+            )
+        );
+      }
+      draftOrder.line_items = draftOrder.line_items.concat([campaignItem]);
+      draftOrder.tags = 'Thunder Exit Upsell Funnel,discount';
+    } else if (mode === 'free_shipping') {
+      const campaignItem = {
+        variant_id: variantId,
+        quantity,
+      };
+      if (sellType === 'upsell') {
+        draftOrder.line_items = draftOrder.line_items.filter(
+          (lineItem) =>
+            !products.find(
+              (product) =>
+                product.legacyResourceId.toString() ===
+                lineItem.product_id.toString()
+            )
+        );
+      }
+      draftOrder.line_items = draftOrder.line_items.concat([campaignItem]);
+      draftOrder.shipping_line = {
+        price: 0.0,
+        title: 'Free Shipping',
+      };
+      draftOrder.tags = 'Thunder Exit Upsell Funnel,free_shipping';
+    } else if (mode === 'gift') {
+      const campaignItem = {
+        variant_id: variantId,
+        quantity,
+        applied_discount: {
+          value: '100',
+          value_type: 'percentage',
+        },
+      };
+      draftOrder.line_items = draftOrder.line_items.concat([campaignItem]);
+      draftOrder.tags = 'Thunder Exit Upsell Funnel,gift';
+    }
+    let order = await fetch(
+      `https://${shop}/admin/api/2021-01/draft_orders.json`,
+      {
+        credentials: 'include',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Shopify-Access-Token': accessToken,
+        },
+        body: JSON.stringify({
+          draft_order: draftOrder,
+        }),
+      }
+    );
+    order = await order.json();
+    if (order && order.draft_order) {
+      const addedVariantItem = order.draft_order.line_items.find(
+        (lineItem) => lineItem.variant_id === parseInt(variantId, 10)
+      );
+      if (addedVariantItem) {
+        const { price, applied_discount, quantity } = addedVariantItem;
+        const variantPrice = parseFloat(price) * parseInt(quantity, 10);
+        const addedValue = applied_discount.amount
+          ? variantPrice - parseFloat(applied_discount.amount)
+          : variantPrice;
+        const { invoice_url, currency, total_price } = order.draft_order;
+        await db.query(
+          `INSERT INTO orders${db.insertColumns(
+            'campaign_id',
+            'domain',
+            'draft_order_id',
+            'currency',
+            'value_added',
+            'total_price'
+          )}`,
+          [id, shop, order.draft_order.id, currency, addedValue, total_price]
+        );
+        ctx.body = { invoiceUrl: invoice_url };
+        ctx.status = 200;
+      }
+    } else {
+      const orderCreationError = new Error(
+        `Failed to create an order for ${shop}.`
+      );
+      reportError(orderCreationError);
+      ctx.status = 500;
+    }
+  } catch (error) {
+    reportError(error);
+    ctx.status = 500;
+  }
+};
+
+export default createDraftOrder;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-/* eslint-disable babel/camelcase */
 import fs from 'fs';
 import https from 'https';
 import path from 'path';
@@ -40,6 +39,7 @@ import {
   registerWebhooks,
 } from './handlers';
 import countView from './controllers/count_view';
+import createDraftOrder from './controllers/create_draft_order';
 
 dotenv.config();
 
@@ -317,130 +317,7 @@ app.prepare().then(() => {
     }
   });
 
-  router.post('/api/create-draft-order', async (ctx) => {
-    try {
-      const {
-        variantId,
-        strategy,
-        quantity,
-        cart,
-        shop,
-        id,
-        products,
-      } = ctx.request.body;
-      const store = await db.query(
-        `SELECT * FROM stores WHERE stores.domain = $1`,
-        [shop]
-      );
-      const accessToken = store.rows[0].access_token;
-      const draftOrder = {
-        line_items: cart.items.map((item) => ({ ...item, properties: [] })),
-      };
-      const { sellType, mode } = strategy;
-      if (mode === 'discount') {
-        const campaignItem = {
-          variant_id: variantId,
-          quantity,
-          applied_discount: {
-            value: strategy.discount.value,
-            value_type:
-              strategy.discount.type === '%' ? 'percentage' : 'fixed_amount',
-          },
-        };
-        if (sellType === 'upsell') {
-          draftOrder.line_items = draftOrder.line_items.filter(
-            (lineItem) =>
-              !products.find(
-                (product) =>
-                  product.legacyResourceId.toString() ===
-                  lineItem.product_id.toString()
-              )
-          );
-        }
-        draftOrder.line_items = draftOrder.line_items.concat([campaignItem]);
-        draftOrder.tags = 'Thunder Exit Upsell Funnel,discount';
-      } else if (mode === 'free_shipping') {
-        const campaignItem = {
-          variant_id: variantId,
-          quantity,
-        };
-        if (sellType === 'upsell') {
-          draftOrder.line_items = draftOrder.line_items.filter(
-            (lineItem) =>
-              !products.find(
-                (product) =>
-                  product.legacyResourceId.toString() ===
-                  lineItem.product_id.toString()
-              )
-          );
-        }
-        draftOrder.line_items = draftOrder.line_items.concat([campaignItem]);
-        draftOrder.shipping_line = {
-          price: 0.0,
-          title: 'Free Shipping',
-        };
-        draftOrder.tags = 'Thunder Exit Upsell Funnel,free_shipping';
-      } else if (mode === 'gift') {
-        const campaignItem = {
-          variant_id: variantId,
-          quantity,
-          applied_discount: {
-            value: '100',
-            value_type: 'percentage',
-          },
-        };
-        draftOrder.line_items = draftOrder.line_items.concat([campaignItem]);
-        draftOrder.tags = 'Thunder Exit Upsell Funnel,gift';
-      }
-      let order = await fetch(
-        `https://${shop}/admin/api/2021-01/draft_orders.json`,
-        {
-          credentials: 'include',
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Shopify-Access-Token': accessToken,
-          },
-          body: JSON.stringify({
-            draft_order: draftOrder,
-          }),
-        }
-      );
-      order = await order.json();
-      if (order && order.draft_order) {
-        const addedVariantItem = order.draft_order.line_items.find(
-          (lineItem) => lineItem.variant_id === parseInt(variantId, 10)
-        );
-        if (addedVariantItem) {
-          const { price, applied_discount, quantity } = addedVariantItem;
-          const variantPrice = parseFloat(price) * parseInt(quantity, 10);
-          const addedValue = applied_discount.amount
-            ? variantPrice - parseFloat(applied_discount.amount)
-            : variantPrice;
-          const { invoice_url, currency, total_price } = order.draft_order;
-          await db.query(
-            `INSERT INTO orders${db.insertColumns(
-              'campaign_id',
-              'domain',
-              'draft_order_id',
-              'currency',
-              'value_added',
-              'total_price'
-            )}`,
-            [id, shop, order.draft_order.id, currency, addedValue, total_price]
-          );
-          ctx.body = { invoiceUrl: invoice_url };
-          ctx.status = 200;
-        }
-      } else {
-        console.error(order);
-        ctx.status = 500;
-      }
-    } catch (error) {
-      ctx.status = 500;
-      console.error(error);
-    }
-  });
+  router.post('/api/create-draft-order', createDraftOrder);
 
   router.post('/api/count-view', countView);
 
