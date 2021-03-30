eb0ed37dd77028fb75b3619ce4921fe06aad793a
Pascal Mayr <pascalmayr2@gmail.com>
2021-03-30 11:26:00 +0200

    notifiying the merchant about 80% and 100% views used

--- server/controllers/count_view.js
+++ server/controllers/count_view.js
@@ -1,4 +1,5 @@
 import db from '../db';
+import sendMail from '../handlers/mail';
 
 import reportError from './report_error';
 
@@ -13,6 +14,46 @@ const countView = async (ctx) => {
       )}`,
       [id, shop, target]
     );
+    let views = await db.query(
+      `SELECT COUNT(*) FROM views WHERE domain = $1 AND date_part('month', views.view_time) = date_part('month', (SELECT current_timestamp))`,
+      [shop]
+    );
+    let limit = await db.query(
+      `SELECT plan_limit FROM stores WHERE domain = $1`,
+      [shop]
+    );
+    const contact = await db.query(
+      `SELECT email, first_name FROM users WHERE domain = $1 AND account_owner = TRUE`,
+      [shop]
+    );
+    views = views.rows[0].count;
+    limit = limit.rows[0].plan_limit;
+    const email = contact.rows[0].email;
+    const name = contact.rows[0].first_name;
+    if (isNaN(views) && isNaN(limit)) {
+      if ((views * 100) / limit === 80) {
+        await sendMail(
+          {
+            to: email,
+          },
+          'planUsed80',
+          {
+            name,
+          }
+        );
+      }
+      if ((views * 100) / limit === 100) {
+        await sendMail(
+          {
+            to: email,
+          },
+          'planUsed100',
+          {
+            name,
+          }
+        );
+      }
+    }
     ctx.status = 200;
   } catch (error) {
     reportError(error);
