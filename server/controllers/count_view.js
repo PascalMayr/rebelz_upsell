6a2fab6410da187b59069af10326401b56b7f2e5
Sascha Mayr <mayr.sascha@gmail.com>
2021-04-01 10:12:17 +0100

    Minor improvements

--- server/controllers/count_view.js
+++ server/controllers/count_view.js
@@ -3,7 +3,7 @@ import db from '../db';
 const countView = async (ctx) => {
   const { id, shop, target } = ctx.request.body;
   await db.query(
-    `INSERT INTO views${db.insertColumns(
+    `INSERT INTO views ${db.insertColumns(
       'campaign_id',
       'domain',
       'target_page'
