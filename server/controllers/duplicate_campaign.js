f26f79b5edfffea011766d99f7e09ae8f6d7a3c9
Pascal Mayr <pascalmayr2@gmail.com>
2021-04-08 15:10:41 +0200

    quick fix showing views still after campaign was duplicated

--- server/controllers/duplicate_campaign.js
+++ server/controllers/duplicate_campaign.js
@@ -16,11 +16,30 @@ const duplicateCampaign = async (ctx) => {
   await db.query(`INSERT INTO campaigns ${db.insertColumns(...columns)}`, [
     ...insertValues,
   ]);
-  const campaigns = await db.query(
-    'SELECT * FROM campaigns WHERE domain = $1',
-    [ctx.session.shop]
+  let campaigns = await db.query('SELECT * FROM campaigns WHERE domain = $1', [
+    ctx.session.shop,
+  ]);
+  campaigns = await Promise.all(
+    campaigns.rows.map(async (campaign) => {
+      const views = await db.query(
+        `SELECT counter FROM views WHERE domain = $1 AND campaign_id = $2 AND date_part('month', views.view_date) = date_part('month', (SELECT current_date))`,
+        [ctx.session.shop, campaign.id]
+      );
+      let viewsCount = 0;
+      if (views.rows.length > 0) {
+        viewsCount = views.rows
+          .map((row) => (row.counter ? parseInt(row.counter, 10) : 0))
+          .reduce((sum, counter) => sum + counter);
+      }
+      return {
+        ...campaign,
+        views: viewsCount,
+        sales: 0,
+        revenue: 0,
+      };
+    })
   );
-  ctx.body = campaigns.rows;
+  ctx.body = campaigns;
   ctx.status = 200;
 };
 
