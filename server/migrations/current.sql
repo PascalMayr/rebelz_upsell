1cff554de2c180696dd9ea52ee6922870b6cec4b
Sascha Mayr <mayr.sascha@gmail.com>
2021-04-11 23:09:04 +0100

    Reset current.sql

--- server/migrations/current.sql
+++ server/migrations/current.sql
@@ -1,5 +1,1 @@
 -- Enter migration here
-ALTER TABLE campaigns
-ALTER COLUMN "created" TYPE timestamp with time zone,
-ALTER COLUMN "updated" TYPE timestamp with time zone,
-ALTER COLUMN "deleted" TYPE timestamp with time zone;
