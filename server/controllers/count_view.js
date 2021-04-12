ad40981a0591c220dc1d7edb87fd83cf1c1f825c
Sascha Mayr <mayr.sascha@gmail.com>
2021-04-12 14:07:27 +0100

    Remove outdated reporterror

--- server/controllers/count_view.js
+++ server/controllers/count_view.js
@@ -2,57 +2,52 @@ import db from '../db';
 import sendMail, { mailTemplates } from '../handlers/mail';
 
 const countView = async (ctx) => {
-  try {
-    const { id, shop, target } = ctx.request.body;
-    await db.query(
-      `INSERT INTO views ${db.insertColumns(
-        'campaign_id',
-        'domain',
-        'target_page'
-      )} ON CONFLICT ON CONSTRAINT campaign_domain_target_date DO UPDATE SET counter = views.counter + 1`,
-      [id, shop, target]
-    );
-    let views = await db.query(
-      `SELECT COUNT(*) FROM views WHERE domain = $1 AND date_part('month', views.view_time) = date_part('month', (SELECT current_timestamp))`,
-      [shop]
-    );
-    let limit = await db.query(
-      `SELECT plan_limit FROM stores WHERE domain = $1`,
-      [shop]
-    );
-    const contact = await db.query(
-      `SELECT email, first_name FROM users WHERE domain = $1 AND account_owner = TRUE`,
-      [shop]
-    );
-    views = views.rows[0].count;
-    limit = limit.rows[0].plan_limit;
-    const email = contact.rows[0].email;
-    const name = contact.rows[0].first_name;
-    if (isNaN(views) && isNaN(limit)) {
-      if ((views * 100) / limit === 80) {
-        await sendMail({
-          to: email,
-          template: mailTemplates.planUsed80,
-          templateData: {
-            name,
-          },
-        });
-      }
-      if ((views * 100) / limit === 100) {
-        await sendMail({
-          to: email,
-          template: mailTemplates.planUsed100,
-          templateData: {
-            name,
-          },
-        });
-      }
+  const { id, shop, target } = ctx.request.body;
+  await db.query(
+    `INSERT INTO views ${db.insertColumns(
+      'campaign_id',
+      'domain',
+      'target_page'
+    )} ON CONFLICT ON CONSTRAINT campaign_domain_target_date DO UPDATE SET counter = views.counter + 1`,
+    [id, shop, target]
+  );
+  let views = await db.query(
+    `SELECT COUNT(*) FROM views WHERE domain = $1 AND date_part('month', views.view_time) = date_part('month', (SELECT current_timestamp))`,
+    [shop]
+  );
+  let limit = await db.query(
+    `SELECT plan_limit FROM stores WHERE domain = $1`,
+    [shop]
+  );
+  const contact = await db.query(
+    `SELECT email, first_name FROM users WHERE domain = $1 AND account_owner = TRUE`,
+    [shop]
+  );
+  views = views.rows[0].count;
+  limit = limit.rows[0].plan_limit;
+  const email = contact.rows[0].email;
+  const name = contact.rows[0].first_name;
+  if (isNaN(views) && isNaN(limit)) {
+    if ((views * 100) / limit === 80) {
+      await sendMail({
+        to: email,
+        template: mailTemplates.planUsed80,
+        templateData: {
+          name,
+        },
+      });
     }
-    ctx.status = 200;
-  } catch (error) {
-    reportError(error);
-    ctx.status = 500;
+    if ((views * 100) / limit === 100) {
+      await sendMail({
+        to: email,
+        template: mailTemplates.planUsed100,
+        templateData: {
+          name,
+        },
+      });
+    }
   }
+  ctx.status = 200;
 };
 
 export default countView;
