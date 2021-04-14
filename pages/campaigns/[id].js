50dde72be86347ebf8b465da6f93c41000377a3e
Sascha Mayr <mayr.sascha@gmail.com>
2021-04-14 23:06:54 +0100

    Refactor state hydration on campaign edit

--- pages/campaigns/[id].js
+++ pages/campaigns/[id].js
@@ -3,28 +3,15 @@ import db from '../../server/db';
 import New from './new';
 
 export async function getServerSideProps(context) {
-  const data = await db.query('SELECT * FROM campaigns WHERE id = $1', [
+  const campaign = await db.query('SELECT * FROM campaigns WHERE id = $1', [
     context.params.id,
   ]);
-  const campaign = data.rows[0];
-  const formatDate = (date) => new Intl.DateTimeFormat('en-US', { dateStyle: 'medium', timeStyle: 'long',hour12: false }).format(date)
   return {
-    props: {
-      campaign: {
-        ...campaign,
-        ...{
-          created: campaign.created
-            ? formatDate(campaign.created)
-            : campaign.created,
-          updated: campaign.updated
-            ? formatDate(campaign.updated)
-            : campaign.updated,
-          deleted: campaign.deleted
-            ? campaign.deleted.toString()
-            : campaign.deleted,
-        },
-      },
-    },
+    props: JSON.parse(
+      JSON.stringify({
+        campaign: campaign.rows[0],
+      })
+    ),
   };
 }
 
