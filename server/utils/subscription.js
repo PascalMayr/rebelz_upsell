8291c177cc1112d1aa4123a6cc34d1083266e2cc
Sascha Mayr <mayr.sascha@gmail.com>
2021-04-14 13:48:17 +0100

    Fix first day of billing cycle calculation

--- server/utils/subscription.js
+++ server/utils/subscription.js
@@ -2,7 +2,7 @@ import { DateTime } from 'luxon';
 
 export const firstDayOfCurrentBillingCycle = (subscriptionStart) => {
   let compareDate = DateTime.fromJSDate(subscriptionStart).startOf('day');
-  const dateToday = DateTime.now().toJSDate().startOf('day');
+  const dateToday = DateTime.now().startOf('day');
   while (dateToday.diff(compareDate, 'days').days >= 30) {
     compareDate = compareDate.minus({ days: 30 });
   }
