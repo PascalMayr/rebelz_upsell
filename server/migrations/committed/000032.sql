8c576a08f56127b6232ecd27ddc1be5cecff3621
Pascal Mayr <pascalmayr2@gmail.com>
2021-04-21 16:54:37 +0200

    fixed migration

--- server/migrations/committed/000032.sql
+++ server/migrations/committed/000032.sql
@@ -1,5 +1,5 @@
---! Previous: sha1:d4c39cac372fa4b632a4ce10b3553833b3257caf
---! Hash: sha1:70816aa9da0a0efdced869d1a1b5efcbead83f6b
+--! Previous: sha1:3bf5f8680d7b676cce52f25aa5fe2d05115a0f2f
+--! Hash: sha1:a7bb6b2c7805ce4dfc322b01cecdb656f72f97d2
 
 -- Enter migration here
 ALTER TABLE ONLY stores ALTER COLUMN enabled SET DEFAULT true;
