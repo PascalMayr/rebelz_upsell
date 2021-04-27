aeb7e34a23e6e7d5d2e79775eaa4892776d17f8a
Sascha Mayr <mayr.sascha@gmail.com>
2021-04-27 17:26:12 +0100

    Switch to useAppBridge

--- components/hooks/use_api.js
+++ components/hooks/use_api.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 import { getSessionToken } from '@shopify/app-bridge-utils';
-import { Context as ShopifyAppContext } from '@shopify/app-bridge-react';
-import { useContext } from 'react';
+import { useAppBridge } from '@shopify/app-bridge-react';
 
 export default function useApi() {
   // https://ux.stackexchange.com/a/56747
   const api = axios.create({
     timeout: 8000,
   });
-  const app = useContext(ShopifyAppContext);
+  const app = useAppBridge();
   api.interceptors.request.use(async (config) => {
     const sessionToken = await getSessionToken(app);
     config.headers.Authorization = `Bearer ${sessionToken}`;
