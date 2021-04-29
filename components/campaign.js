b73a574f39dfdb6ee330a7968075e80e989a80bc
Sascha Mayr <mayr.sascha@gmail.com>
2021-04-29 16:44:26 +0100

    Remove campaign labels except Published/Unpublished

--- components/campaign.js
+++ components/campaign.js
@@ -1,22 +1,8 @@
 import { Badge, TextStyle, Icon } from '@shopify/polaris';
-import {
-  CartMajor,
-  HeartMajor,
-  ProductsMajor,
-  ViewMajor,
-  ReplaceMajor,
-  CartUpMajor,
-  ShipmentMajor,
-  DiscountsMajor,
-  GiftCardMajor,
-  WandMajor,
-} from '@shopify/polaris-icons';
+import { ViewMajor } from '@shopify/polaris-icons';
 import '../styles/components/campaign.css';
 
 const Campaign = ({ campaign }) => {
-  const page = campaign.targets.page;
-  const sellType = campaign.strategy.sellType;
-  const mode = campaign.strategy.mode;
   const publishedStatus = campaign.published ? 'success' : 'attention';
   const publishedStatusText = campaign.published
     ? ' Published'
@@ -31,66 +17,6 @@ const Campaign = ({ campaign }) => {
         <Icon source={ViewMajor} />
         {publishedStatusText} {views} Views
       </Badge>
-      <Badge status="info">
-        {page === 'add_to_cart' && (
-          <>
-            <Icon source={ProductsMajor} />
-            {' Product Page'}
-          </>
-        )}
-        {page === 'checkout' && (
-          <>
-            <Icon source={CartMajor} />
-            {' Cart Page'}
-          </>
-        )}
-        {page === 'thank_you' && (
-          <>
-            <Icon source={HeartMajor} />
-            {' Thank you Page'}
-          </>
-        )}
-      </Badge>
-      <Badge>
-        {sellType === 'upsell' && (
-          <>
-            <Icon source={ReplaceMajor} />
-            Upsell
-          </>
-        )}
-        {sellType === 'cross_sell' && (
-          <>
-            <Icon source={CartUpMajor} />
-            Cross sell
-          </>
-        )}
-      </Badge>
-      <Badge status="warning">
-        {mode === 'free_shipping' && (
-          <>
-            <Icon source={ShipmentMajor} />
-            Free Shipping
-          </>
-        )}
-        {mode === 'discount' && (
-          <>
-            <Icon source={DiscountsMajor} />
-            Discount
-          </>
-        )}
-        {mode === 'gift' && (
-          <>
-            <Icon source={GiftCardMajor} />
-            Gift
-          </>
-        )}
-      </Badge>
-      {campaign.selling.mode === 'auto' && (
-        <Badge>
-          <Icon source={WandMajor} />
-          Auto Recommendations
-        </Badge>
-      )}
     </>
   );
 };
