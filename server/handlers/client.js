9045dfc92460ad9d8b7003ef968d3eed26e07f7d
Sascha Mayr <mayr.sascha@gmail.com>
2021-05-04 14:34:46 +0100

    Use the apollo client instead of deprecated apollo boost

--- server/handlers/client.js
+++ server/handlers/client.js
@@ -1,15 +1,12 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const createClient = (shop, accessToken) => {
   return new ApolloClient({
-    uri: `https://${shop}/admin/api/2020-10/graphql.json`,
-    request: (operation) => {
-      operation.setContext({
-        headers: {
-          'X-Shopify-Access-Token': accessToken,
-          'User-Agent': `shopify-app-node ${process.env.npm_package_version} | Shopify App CLI`,
-        },
-      });
-    },
+    uri: `https://${shop}/admin/api/2021-04/graphql.json`,
+    cache: new InMemoryCache(),
+    headers: {
+      'X-Shopify-Access-Token': accessToken,
+      'User-Agent': `shopify-app-node ${process.env.npm_package_version} | Shopify App CLI`,
+    }
   });
 };
