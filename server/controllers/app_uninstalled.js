cdb0cdec45dca2c81b288c4b3e9e8a6a33c1ac81
Sascha Mayr <mayr.sascha@gmail.com>
2021-05-21 16:00:51 +0100

    Reset subscription plan when app gets uninstalled

--- server/controllers/app_uninstalled.js
+++ server/controllers/app_uninstalled.js
@@ -2,7 +2,13 @@ import db from '../db';
 
 const appUninstalled = async (ctx) => {
   const shop = ctx.request.headers['x-shopify-shop-domain'];
-  await db.query('UPDATE stores SET enabled = false WHERE domain = $1', [shop]);
+  const freePlan = config.plans.find(
+    (configPlan) => configPlan.name === config.planNames.free
+  );
+  await db.query(
+    'UPDATE stores SET enabled = false, plan_name = $1, "subscriptionId" = NULL, plan_limit = $2 WHERE domain = $1',
+    [freePlan.name, freePlan.limit, shop]
+  );
 
   ctx.body = {};
   ctx.status = 200;
