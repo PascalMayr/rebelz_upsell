9db849871e18fffb59eb2561dd53f5cf80d857e6
Sascha Mayr <mayr.sascha@gmail.com>
2021-05-25 13:26:55 +0100

    Debug first sale email

--- server/controllers/draft_orders_update.js
+++ server/controllers/draft_orders_update.js
@@ -5,13 +5,14 @@ import sendMail, { mailTemplates } from '../handlers/mail';
 
 const draftOrdersUpdate = async (ctx) => {
   const shop = ctx.request.headers['x-shopify-shop-domain'];
-  const { id, status, order_id } = ctx.request.body;
+  const { id, status, order_id: orderId } = ctx.request.body;
   const completedStatus = 'completed';
 
   let completedOrderCount = await db.query(
     'SELECT COUNT(*) FROM orders WHERE domain = $1 AND status = $2;',
     [shop, completedStatus]
   );
+  console.dir(completedOrderCount);
   completedOrderCount = completedOrderCount.rows[0].count;
 
   if (status === completedStatus) {
@@ -22,7 +23,7 @@ const draftOrdersUpdate = async (ctx) => {
     store = store.rows[0];
     const client = new Shopify.Clients.Rest(shop, store.access_token);
     const orderData = await client.get({
-      path: `orders/${order_id}`,
+      path: `orders/${orderId}`,
     });
 
     await db.query(
@@ -31,10 +32,18 @@ const draftOrdersUpdate = async (ctx) => {
     );
 
     if (completedOrderCount === 0) {
+      console.log('if completed order count 0');
       const contact = await db.query(
         `SELECT email, first_name FROM users WHERE domain = $1 AND account_owner = TRUE`,
         [shop]
       );
+      console.dir({
+        to: contact.rows[0].email,
+        template: mailTemplates.firstSale,
+        templateData: {
+          name: contact.rows[0].first_name,
+        },
+      });
       await sendMail({
         to: contact.rows[0].email,
         template: mailTemplates.firstSale,
