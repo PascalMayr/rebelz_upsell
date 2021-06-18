17e3344b984c04b1c0ab9eef72bd787f94f38483
Sascha Mayr <mayr.sascha@gmail.com>
2021-06-18 10:56:58 +0100

    Remove the deprecated next css plugin that is automatically included in next js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 const { parsed: localEnv } = require('dotenv').config();
-const withCSS = require('@zeit/next-css');
 const webpack = require('webpack');
 
 const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
 const nodeEnv = JSON.stringify(process.env.NODE_ENV);
 const sentryEnv = JSON.stringify(process.env.SENTRY_DSN_DASHBOARD);
 
-module.exports = withCSS({
+module.exports = {
   webpack: (config) => {
     const env = { API_KEY: apiKey, NODE_ENV: nodeEnv, SENTRY_DSN_DASHBOARD: sentryEnv};
     config.plugins.push(new webpack.DefinePlugin(env));
     return config;
   },
-});
+};
