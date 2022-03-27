e298f2b227099394c13ed9dd2a39b30f767b282e
Pascal Mayr <pascalmayr2@gmail.com>
2022-03-27 21:54:11 +0700

    fix authentication for development

--- server/handlers/client.js
+++ server/handlers/client.js
@@ -1,12 +1,23 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const createClient = (shop, accessToken) => {
+  if (
+    process.env.NODE_ENV === 'development' &&
+    !process.env.PRIVATE_ACCESS_TOKEN
+  ) {
+    throw new Error(
+      'PRIVATE_ACCESS_TOKEN env missing. Go to the dev store, create a private App, get the access token and set the missing env.'
+    );
+  }
   return new ApolloClient({
     uri: `https://${shop}/admin/api/2021-04/graphql.json`,
     cache: new InMemoryCache(),
     headers: {
-      'X-Shopify-Access-Token': accessToken,
+      'X-Shopify-Access-Token':
+        process.env.NODE_ENV === 'development'
+          ? process.env.PRIVATE_ACCESS_TOKEN
+          : accessToken,
       'User-Agent': `shopify-app-node ${process.env.npm_package_version} | Shopify App CLI`,
-    }
+    },
   });
 };
